7b8be8a765a27e8612cacf77b2c3770716f41389
deid84 <d.ferrero@me.com>
2025-10-22 20:33:12 +0200

    fixed wrong port in vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
     plugins: [
         vue(),
@@ -10,6 +13,7 @@ export default defineConfig({
             include: path.resolve(__dirname, './src/locales/**'),
         }),
     ],
+    clearScreen: false,
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
@@ -18,4 +22,20 @@ export default defineConfig({
     optimizeDeps: {
         include: [],
     },
+    server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
 });
